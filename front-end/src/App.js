5c5af50e854883cef4c964036a335409540276d7
Sijia1998 <zhu19980304@163.com>
2019-02-11 00:46:12 +0800

    add react-router-dom

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import { Provider } from 'mobx-react';
 import Layout from './components/layout';
 import store from './store';
-
 import styles from './App.less';
 
 class App extends Component {
   render() {
-    return <div className={styles['content-inner']}>
+    return <div className={styles['content-inner']}  className="wrapper">
       <Provider store={store}>
         <Layout>123</Layout>
       </Provider>
--- front-end/src/components/layout/index.js
+++ front-end/src/components/layout/index.js
@@ -1,11 +1,20 @@
 import React, { Component, Fragment } from 'react';
+import {HashRouter as Router,Route,Switch} from 'react-router-dom';
 import Header from '../header/header';
+import ReactMarkDown from '../markdown/markdown';
 
 class Layout extends Component {
   render() {
-    return <Fragment>
-      <Header></Header>
-    </Fragment>;
+    return <div>
+      <Router>
+        <div>
+          <Route path="/" component={Header}></Route>
+          <Route path="/editor" component={ReactMarkDown}></Route>
+        </div>
+      </Router>
+      {/* <Header></Header> 
+      <ReactMarkDown></ReactMarkDown> */}
+    </div>
   }
 }
 export default Layout;
